c6895480ee58eadb8c36282b0992407f557642c6
Tasnim <tasnim,ahmed@powercoders.org>
2019-04-05 14:25:34 +0300

    cons

--- scratch/shopping/shopping.js
+++ scratch/shopping/shopping.js
@@ -1,13 +1,13 @@
-let inputBox = document.getElementById('item');
-let shoppinglist = document.querySelector('ul')
-let button = document.querySelector('button');
+const inputBox = document.getElementById('item');
+const shoppinglist = document.querySelector('ul')
+const button = document.querySelector('button');
 
 button.disabled = true;
 
 button.addEventListener('click', function (event) {
     if (inputBox.value.trim() !== '') {
         button.disabled = false;
-        let li = creatNewListItem(inputBox.value);
+        const li = creatNewListItem(inputBox.value);
         shoppinglist.appendChild(li);
         inputBox.value = '';
         button.disabled = true
@@ -18,7 +18,7 @@ inputBox.addEventListener('keyup', function (event) {
     if (inputBox.value.trim() !== '') {
         button.disabled = false;
         if (event.key === 'Enter') {
-            let li = creatNewListItem(inputBox.value);
+            const li = creatNewListItem(inputBox.value);
             shoppinglist.appendChild(li);
             inputBox.value = '';
             button.disabled = true
@@ -29,14 +29,14 @@ inputBox.focus();
 
 function creatNewListItem(itemName) {
 
-    let element = document.createElement('li');
+    const element = document.createElement('li');
 
-    let span = document.createElement('span');
+    const span = document.createElement('span');
     span.innerText = itemName;
 
-    let space = document.createTextNode('  ');
+    const space = document.createTextNode('  ');
 
-    let click = document.createElement('button');
+    const click = document.createElement('button');
     click.innerText = "Delete";
 
     click.addEventListener('click', function (event) {
